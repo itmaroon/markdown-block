85e7279ef2f9ec5349b432bd1b8c1511b12f419a
itmaroon <master@itmaroon.net>
2023-07-15 11:57:40 +0900

    7-15

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '92ac8b2aef004ca04b2d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8ae066de833a2270f22');
