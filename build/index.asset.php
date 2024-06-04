898b078c1199b40079f902bddc3719f64c5723ae
itmaroon <itmaroon@icloud.com>
2024-06-04 16:55:24 +0900

    6-4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c179309b437dfdc2c4f2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c2bfea03f337b986f858');
